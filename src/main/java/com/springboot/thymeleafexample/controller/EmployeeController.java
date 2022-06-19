968347bcac0f40e431a1c968e2327d9d0ff2c445
Admin <volam130601@gmail.com>
2022-06-19 10:03:02 +0700

    Two Commit

--- src/main/java/com/springboot/thymeleafexample/controller/EmployeeController.java
+++ src/main/java/com/springboot/thymeleafexample/controller/EmployeeController.java
@@ -46,7 +46,7 @@ public class EmployeeController {
     @GetMapping("/deleteEmployee/{id}")
     public String deleteThroughId(@PathVariable(value = "id") long id) {
         employeeService.deleteViaId(id);
-        //Hello
+        //Hello 2
         return "redirect:/";
     }
 }
